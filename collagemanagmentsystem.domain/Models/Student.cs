21e5dbcc859dd68034572819b1c61ffcbe8ffb19
sinteco <centg2g@gmail.com>
2022-04-30 22:29:56 +0300

    first commit

---
+++ collagemanagmentsystem.domain/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collagemanagmentsystem.domain.Models
+{
+    internal class Student
+    {
+    }
+}
---
+++ collagemanagmentsystem/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace collagemanagmentsystem.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
